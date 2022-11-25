be03be6c6334d7c156212f009b52d3cbcb525e1a
Soomin Lim <augustth511@gmail.com>
2022-11-25 21:26:27 +0900

    test(LottoPurchaseMoneyTest): 로또 구입 금액만큼 로또를 발행하는지 개수를 검증하는 테스트 코드 작성

--- src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
+++ src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
@@ -1,10 +1,14 @@
 package lotto.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
 
 class LottoPurchaseMoneyTest {
 
@@ -22,5 +26,15 @@ class LottoPurchaseMoneyTest {
     void createLottoPurchaseMoneyByInvalidUnitNumber(Integer input) {
         assertThatThrownBy(() -> new LottoPurchaseMoney(input))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("로또 구입 금액만큼 로또를 발행하는지 개수를 검증한다.")
+    @Test
+    void getLottoNumber() {
+        LottoPurchaseMoney lottoPurchaseMoney = new LottoPurchaseMoney(3000);
+
+        int result = lottoPurchaseMoney.getLottoNumber();
+
+        assertThat(result).isEqualTo(3);
     }
 }
