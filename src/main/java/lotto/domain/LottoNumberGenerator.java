196d088b03e404a06639d07bdd73812e109dfd91
Soomin Lim <augustth511@gmail.com>
2022-11-25 19:52:06 +0900

    fix(LottoNumberGenerator): 로또 번호가 오름차순으로 정렬되도록 수정

--- src/main/java/lotto/domain/LottoNumberGenerator.java
+++ src/main/java/lotto/domain/LottoNumberGenerator.java
@@ -1,12 +1,16 @@
 package lotto.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator {
 
     public static List<Integer> generate() {
-        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(Lotto.MINIMUM_NUMBER, Lotto.MAXIMUM_NUMBER, Lotto.NUMBERS_SIZE);
+        List<Integer> numbers = new ArrayList<>(Randoms.pickUniqueNumbersInRange(Lotto.MINIMUM_NUMBER, Lotto.MAXIMUM_NUMBER, Lotto.NUMBERS_SIZE));
+        Collections.sort(numbers);
         return numbers;
     }
 }
