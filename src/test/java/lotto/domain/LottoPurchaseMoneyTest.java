de6dd04309074bd68fe17c1d274063089796bbb6
Soomin Lim <augustth511@gmail.com>
2022-11-25 18:57:01 +0900

    test(LottoPurchaseMoneyTest): 로또 구입 금액이 1,000 미만인 경우 예외 처리하는 테스트 코드 작성

--- src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
+++ src/test/java/lotto/domain/LottoPurchaseMoneyTest.java
@@ -3,13 +3,24 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class LottoPurchaseMoneyTest {
+
+    @DisplayName("로또 구입 금액이 1,000 미만인 경우 예외가 발생한다.")
+    @ValueSource(ints = {-123, 0, 999})
+    @ParameterizedTest
+    void createLottoPurchaseMoneyByNumberOfLessThanPrice(Integer input) {
+        assertThatThrownBy(() -> new LottoPurchaseMoney(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @DisplayName("로또 구입 금액이 1,000으로 나누어 떨어지지 않으면 예외가 발생한다.")
-    @Test
-    void createLottoPurchaseMoneyByInvalidNumber() {
-        assertThatThrownBy(() -> new LottoPurchaseMoney(1500))
+    @ValueSource(ints = {1001, 1500})
+    @ParameterizedTest
+    void createLottoPurchaseMoneyByInvalidUnitNumber(Integer input) {
+        assertThatThrownBy(() -> new LottoPurchaseMoney(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
